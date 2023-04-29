60e36323a72030836a44d84b39b9cc1e5204ba1b
pathaksoham2003 <120130000+pathaksoham2003@users.noreply.github.com>
2023-04-29 15:19:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About.js";
 import Site from "./components/Site.js";
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path = "/ugli17" element = {<Root/>}>
+    <Route path = "/ugli17/" element = {<Root/>}>
       <Route index element = {<Home/>}/>
       <Route path = "products" element = {<Products/>}/>
       <Route path = "about" element = {<About/>}/>
